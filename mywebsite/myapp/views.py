c0033d2e545ab5a7b698d4c5d36471ddad90fa39
rcxscream <llvivixstarll@yahoo.com>
2023-04-23 15:25:24 +0700

    change 'HttpResponse'>'render'
    return render(request,'myapp/homepage.html')

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Homepage(request):
-    return HttpResponse('<h1>HELLO HA HA HA</h>')
+  return render(request,'myapp/homepage.html')
